0be7c9258725a696764b722c2e97a7510418ed78
Jack Nash <jacknash42@gmail.com>
2023-07-15 06:18:43 +1000

    error handling on initial cache

--- src/database.ts
+++ src/database.ts
@@ -20,16 +20,20 @@ export const cache: Cache = {
 };
 
 export async function updateCache() {
-	const abilityNames = (await prisma.ability.findMany({ select: { name: true } })).map((a) => a.name);
-	const perkNames = (await prisma.perk.findMany({ select: { name: true } })).map((p) => p.name);
-	const itemNames = (await prisma.item.findMany({ select: { name: true } })).map((i) => i.name);
-	const roleNames = (await prisma.role.findMany({ select: { name: true } })).map((r) => r.name);
-	const statusNames = (await prisma.status.findMany({ select: { name: true } })).map((s) => s.name);
+	try {
+		const abilityNames = (await prisma.ability.findMany({ select: { name: true } })).map((a) => a.name);
+		const perkNames = (await prisma.perk.findMany({ select: { name: true } })).map((p) => p.name);
+		const itemNames = (await prisma.item.findMany({ select: { name: true } })).map((i) => i.name);
+		const roleNames = (await prisma.role.findMany({ select: { name: true } })).map((r) => r.name);
+		const statusNames = (await prisma.status.findMany({ select: { name: true } })).map((s) => s.name);
 
-	cache.abilities = abilityNames;
-	cache.perks = perkNames;
-	cache.items = itemNames;
-	cache.roles = roleNames;
-	cache.statuses = statusNames;
-	cache.lastUpdated = new Date();
+		cache.abilities = abilityNames;
+		cache.perks = perkNames;
+		cache.items = itemNames;
+		cache.roles = roleNames;
+		cache.statuses = statusNames;
+		cache.lastUpdated = new Date();
+	} catch (err) {
+		console.log(err);
+	}
 }
