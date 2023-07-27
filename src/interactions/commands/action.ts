563c8a01992ec5bad9365fa18a26e7a11ee6f2a9
Jack Nash <jacknash42@gmail.com>
2023-07-27 23:38:15 +1000

    random setup

--- src/interactions/commands/action.ts
+++ src/interactions/commands/action.ts
@@ -1,11 +1,19 @@
 import { ActionRow, ActionRowBuilder, ModalBuilder, SlashCommandBuilder, TextInputBuilder, TextInputStyle } from 'discord.js';
 import { newSlashCommand } from '../../structures/BotClient';
-import { cache } from '../../database';
+import { cache, prisma } from '../../database';
 import config from '../../config';
+import { getActionBacklog, getInventory } from '../../util/database';
+import { formatActionBacklog } from '../../util/embeds';
 
 const data = new SlashCommandBuilder().setName('action').setDescription('Submit an action');
 
-data.addSubcommand((sub) => sub.setName('custom').setDescription('Submit a custom action, ONLY USE when nothing else works'));
+data.addSubcommand((sub) =>
+	sub
+		.setName('custom')
+		.setDescription('Submit a custom action, ONLY USE when nothing else works')
+		.addStringOption((opt) => opt.setName('actions').setDescription('The action/s to submit').setRequired(true))
+		.addIntegerOption((opt) => opt.setName('timestamp').setDescription('FOR AUTOS -> Set a timestamp to perform this action').setRequired(false))
+);
 
 export default newSlashCommand({
 	data,
@@ -16,15 +24,43 @@ export default newSlashCommand({
 
 		const subcommand = i.options.getSubcommand(true);
 
-		switch (subcommand) {
-			case 'custom':
-				const modal = new ModalBuilder();
-				modal.setTitle('Custom Action');
-				modal.setCustomId('submit-custom-action');
-				modal.addComponents(
-					new ActionRowBuilder<TextInputBuilder>().addComponents(new TextInputBuilder().setLabel('Action/s').setStyle(TextInputStyle.Paragraph).setRequired(true).setCustomId('actions'))
-				);
-				return i.showModal(modal);
+		await i.deferReply({ ephemeral: false });
+
+		const inventory = await getInventory({ channelId: i.channelId });
+		if (!inventory) return i.editReply({ content: 'You need to be a player to use this command' });
+
+		const actionBacklog =
+			(await getActionBacklog(i.channelId)) ??
+			(await prisma.actionBacklog.create({
+				data: {
+					channelId: i.channelId,
+					discordId: i.user.id,
+					inventory: {
+						connect: {
+							id: inventory.id,
+						},
+					},
+				},
+			}));
+
+		if (subcommand === 'custom') {
+			const actions = i.options.getString('actions', true);
+			const timestamp = i.options.getInteger('timestamp', false);
+
+			await prisma.pendingAction.create({
+				data: {
+					actionBacklogId: actionBacklog.id,
+					isPlainText: true,
+					action: actions,
+					actionType: 'CUSTOM',
+					timestamp: timestamp ? new Date(timestamp * 1000) : undefined,
+				},
+			});
 		}
+
+		const newBacklog = await getActionBacklog(i.channelId);
+		if (!newBacklog) return i.editReply({ content: 'An error occured' });
+		const { embed, row } = formatActionBacklog(newBacklog);
+		await i.editReply({ embeds: [embed], components: [row] });
 	},
 });
